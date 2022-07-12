26e0254d5dd684914c1245997cc46e34b258567e
rhenea <rheneadianne@gmail.com>
2022-07-12 19:42:55 -0400

    added employee test

---
+++ __tests__/employee.test.js
@@ -0,0 +1,32 @@
+const employee = require('./../lib/employee')
+
+test('creates employee object from employee class based on user input', () => {
+    // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+    // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+    // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+ 
+    let newEmployee = new employee('Firsto Lasto', '6456-M', 'onmyknees@begging.com')
+    expect(newEmployee.name).toEqual(expect.any(String))
+    expect(newEmployee.id).toEqual(expect.any(String))
+    expect(newEmployee.email).toEqual(expect.any(String))
+});
+
+test('function gets employee name from created object', () => {
+    let newEmployee = new employee('Firsto Lasto', '6456-M', 'onmyknees@begging.com')
+    expect(newEmployee.employeeName()).toEqual(expect.any(String))
+})
+
+test('function gets employee id from created object', () => {
+    let newEmployee = new employee('Firsto Lasto', '6456-M', 'onmyknees@begging.com')
+    expect(newEmployee.employeeID()).toEqual(expect.any(String))
+})
+
+test('function gets employee email from created object', () => {
+    let newEmployee = new employee('Firsto Lasto', '6456-M', 'onmyknees@begging.com')
+    expect(newEmployee.employeeEmail()).toEqual(expect.any(String))
+})
+
+test('function gets employee role from created object', () => {
+    let newEmployee = new employee('Firsto Lasto', '6456-M', 'onmyknees@begging.com')
+    expect(newEmployee.employeeRole()).toEqual('Employee')
+})
