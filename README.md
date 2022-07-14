2a3541590f5f719fd2d70d5633586f44320871b9
rhenea <rheneadianne@gmail.com>
2022-07-14 00:42:06 -0400

    comments and readMe added

---
+++ README.md
@@ -0,0 +1,36 @@
+#Team Profile Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    
+##Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributions](#contributions)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+
+## Description
+Javascript application that generates a developed Team Profile page without the user coding a line of HTML or CSS
+
+## Installation
+Install Node.js if you don't have it on your system. Afterwards, download, pull, or fork the repository. Once in your editor of choice, run npm i inquirer@8.2.4 and npm i jest in your terminal. 
+
+## Usage 
+First, ensure you're in the correct directory. Next, run 'node index.js' in your terminal or command line.
+
+## Contributing 
+Project is free to pull and fork! No need to contact for requests.
+
+## Tests
+In the main directory run 'npm run test' All tests should pass to ensure that the application is functioning as espected.
+    
+## GitHub and Contact
+GitHub: [github.com/rheneadianne](https://github.com/rheneadianne)
+Email:  [rheneadianne@gmail.com](mailto:rheneadianne@gmail.com)
+
+## Contribution
+Made with ❤️ by rhenea
+    
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let validation = response => { // validation checker to prevent blank responses
     }
 }
 
-let emailValidation = response => {
+let emailValidation = response => { // checks for proper email format
     if(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(response) === false) {
         console.log(" <---- is an invalid email! Please to enter the email in this format: example@domain.com")
         return false
@@ -26,7 +26,7 @@ let emailValidation = response => {
     }
 }
 
-let numberValidation = response => {
+let numberValidation = response => { // ensures there's at least 1 digit in there and that the string is at list 10 characters long like a phone number. Users can still add other characters like () - or letters for extra info
     if(/\d/.test(response) === false){
         console.log(" <---- does not contain any digits. Please enter a 10 digit number followed by the extension if necessary.") 
         return false
@@ -39,30 +39,30 @@ let numberValidation = response => {
     }
 }
 
-let team =[]
+let team =[] // empty team array
 const addManager = () => {
     return inquirer
         .prompt([
             {
-                type: 'input',
+                type: 'input', // asks managers name
                 name: 'managerName',
                 message: "What is the team Manager's name?",
                 validate: validation
             },
             {
-                type: 'input',
+                type: 'input', // asks managers ID. string so people can enter any character if their ID isnt numerical
                 name: 'managerID',
                 message: "What is the team Manager's employee ID?",
                 validate: validation
             },
             {
-                type: 'input',
+                type: 'input', // asks managers email
                 name: 'managerEmail',
                 message: "What is the team Manager's email?",
                 validate: emailValidation
             },
             {
-                type: 'input',
+                type: 'input', // asks managers phone number
                 name: 'managerNum',
                 message: "What is the team Manager's office number? If they have an extension, please enter ext. [XYZ] after the phone number. ",
                 validate: numberValidation
@@ -79,61 +79,61 @@ const addManager = () => {
 const addEmployee = async () => {
     const questions = [
         {
-            type: 'list',
+            type: 'list', // select role of employee
             name: 'employeeRole',
             message: "Please select the role of the Employee you would like to add.",
             choices: ['Engineer', 'Intern']
         },
         {
-            type: 'input',
+            type: 'input', // employee name
             name: 'employeeName',
             message: "What is the Employee's name?",
             validate: validation
         },
         {
-            type: 'input',
+            type: 'input', // employee ID
             name: 'employeeID',
             message: "What is theEmployee's ID?",
             validate: validation
         },
         {
             type: 'input',
-            name: 'employeeEmail',
+            name: 'employeeEmail', // employee email
             message: "What is the Employee's email?",
             validate: emailValidation
         },
         {
             type: 'input',
-            name: 'employeeGit',
+            name: 'employeeGit', // engineer git
             message: "What is the Employee's GitHub?",
-            when: input => input.employeeRole === "Engineer",
+            when: input => input.employeeRole === "Engineer", // only shows up if Engineer role is selected
             validate: validation
         },
         {
             type: 'input',
-            name: 'employeeSchool',
-            message: "What school is the employee currently attending?",
-            when: input => input.employeeRole === "Intern",
+            name: 'employeeSchool', // intern school
+            message: "What school is the employee currently attending?", 
+            when: input => input.employeeRole === "Intern", // only shows up if intern role is selected
             validate: validation
         },
         {
             type: 'confirm',
             name: 'addMore',
-            message: "Do you have more employees to add?",
+            message: "Do you have more employees to add?", // prompt to loop back to start
             default: true
         }
     ];
     
-    const { addMore, ...answers} = await inquirer.prompt(questions)
+    const { addMore, ...answers} = await inquirer.prompt(questions) // async that waits for inquirer inputs.
     
     if (answers.employeeRole === 'Engineer') {
         const {employeeName, employeeID, employeeEmail, employeeGit} = answers
         const engineer = new Engineer (employeeName, employeeID, employeeEmail, employeeGit)
-        team.push(engineer)
+        team.push(engineer) // pushes created object to team array
     } else {
         const {employeeName, employeeID, employeeEmail, employeeSchool} = answers
         const intern = new Intern (employeeName, employeeID, employeeEmail, employeeSchool)
-        team.push(intern)
+        team.push(intern) // pushes created object to team array
     }
     
     return addMore ? addEmployee(team): team
@@ -142,15 +142,15 @@ const addEmployee = async () => {
 let dataHTML = []
 
 const htmlData = async () => {
-    const inputs = await addEmployee();
-    return dataHTML = inputs
+    const inputs = await addEmployee(); // waits for addEmployee to finish after all the loops
+    return dataHTML = inputs // returns inputs
 }
 
 
 addManager()
     .then(htmlData)
     .then(data => { //creates a new file in dist folder called index.html
-        data = dataHTML
+        data = dataHTML // uses inputs for HTML creation data in line 163
         console.log("Please see your new index.html file in the /dist/index.html folder")
         let dir = path.join(__dirname, 'dist')
         fs.mkdir(dir, (error) => {
